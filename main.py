32d857cff65032cda4466a9dffdcc5fccfcfb8c8
Eric Zhang <ekzhang1@gmail.com>
2022-11-11 17:15:23 -0500

    Add embeddings generation code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import modal
+import json
+import time
 
+import modal
 import numpy as np
 
 stub = modal.Stub()
@@ -23,46 +25,102 @@ if stub.is_inside(stub.clip_image):
     model.eval()
 
 
-@stub.function(image=stub.clip_image, shared_volumes={"/root/.cache": sv})
-def run_clip(*, texts: list[str] = [], image_urls: list[str] = []):
-    """Run pretrained CLIP on a list of texts or image URLs.
+@stub.function(
+    image=stub.clip_image,
+    shared_volumes={"/root/.cache": sv},
+    concurrency_limit=20,
+)
+def run_clip_text(texts: list[str]):
+    """Run pretrained CLIP on a list of texts.
 
     Returns a numpy array containing the concatenated 512-dimensional embedding
     outputs for each provided input, evaluated as a batch.
     """
-    if texts and image_urls:
-        raise ValueError("Only one of texts or image_urls can be provided.")
+    text_tokens = clip.tokenize(texts, truncate=True)
+    with torch.no_grad():
+        return model.encode_text(text_tokens).float().numpy()
 
-    if texts:
-        text_tokens = clip.tokenize(texts)
-        with torch.no_grad():
-            return model.encode_text(text_tokens).float().numpy()
-    elif image_urls:
-        from io import BytesIO
-        from concurrent.futures import ThreadPoolExecutor
 
-        import requests
-        from PIL import Image
+@stub.function(
+    image=stub.clip_image,
+    shared_volumes={"/root/.cache": sv},
+    concurrency_limit=20,
+)
+def run_clip_images(image_urls: list[str]):
+    """Run pretrained CLIP on a list of image URLs.
 
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            responses = list(executor.map(requests.get, image_urls))
+    Returns a numpy array containing the concatenated 512-dimensional embedding
+    outputs for each provided input, evaluated as a batch.
 
-        original_images = [Image.open(BytesIO(resp.content)) for resp in responses]
-        images: list[torch.Tensor] = [preprocess(img) for img in original_images]  # type: ignore
-        image_input = torch.stack(images)
-        with torch.no_grad():
-            return model.encode_image(image_input).float().numpy()
-    else:
-        return np.empty((0, 512))
+    The first return value is a list of indices that had an error during fetch.
+    """
+    from io import BytesIO
+    from concurrent.futures import ThreadPoolExecutor
+
+    import requests
+    from PIL import Image, UnidentifiedImageError
+
+    def get_with_retry(url: str) -> requests.Response:
+        request_num = 0
+        while request_num < 5:
+            try:
+                resp = requests.get(url, timeout=8)
+            except requests.exceptions.RequestException as exc:
+                print("Retrying", url, "due to", exc)
+                request_num += 1
+                time.sleep(3.0)
+                continue
+            if resp.status_code not in (200, 403, 404):
+                print("Retrying", url, "due to status code", resp.status_code)
+                request_num += 1
+                time.sleep(0.1)
+            else:
+                return resp
+        return resp  # type: ignore
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        responses = list(executor.map(get_with_retry, image_urls))
+
+    missing_indices: list[int] = []
+    original_images: list[Image.Image] = []
+    for i, resp in enumerate(responses):
+        if resp.status_code != 200:
+            print(f"Received status code {resp.status_code} from URL:", image_urls[i])
+            missing_indices.append(i)
+            continue
+        try:
+            original_images.append(Image.open(BytesIO(resp.content)))
+        except UnidentifiedImageError:
+            print("Failed to load image from URL:", image_urls[i])
+            missing_indices.append(i)
+    images: list[torch.Tensor] = [preprocess(img) for img in original_images]  # type: ignore
+    image_input = torch.stack(images)
+    with torch.no_grad():
+        return missing_indices, model.encode_image(image_input).float().numpy()
 
 
 if __name__ == "__main__":
+    with open("data/artmuseums-clean.json", "r") as f:
+        data = json.load(f)
+
+    chunk_size = 32
+    chunked_ids = []
+    chunked_urls = []
+    for idx in range(0, len(data), chunk_size):
+        chunked_ids.append([row["id"] for row in data[idx : idx + chunk_size]])
+        chunked_urls.append([row["image_url"] for row in data[idx : idx + chunk_size]])
+
     with stub.run():
-        print(
-            run_clip(
-                image_urls=[
-                    "https://nrs.harvard.edu/urn-3:HUAM:COIN00538_dynmc",
-                    "https://nrs.harvard.edu/urn-3:HUAM:COIN00538_dynmc",
-                ]
-            )
-        )
+        results = list(run_clip_images.map(chunked_urls))
+
+    all_embeddings: dict[str, np.ndarray] = {}
+    for ids, (missing, embeddings) in zip(chunked_ids, results):
+        assert len(ids) == len(missing) + len(embeddings)
+        embeddings_idx = 0
+        for i, id in enumerate(ids):
+            if i not in missing:
+                all_embeddings[str(id)] = embeddings[embeddings_idx, :]
+                embeddings_idx += 1
+
+    print(f"Finished embedding {len(all_embeddings)} images out of {len(data)}")
+    np.savez_compressed("data/embeddings.npz", **all_embeddings)
