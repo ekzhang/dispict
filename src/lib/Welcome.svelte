7e93f062ce66077f6683542344670165769cf4df
Eric Zhang <ekzhang1@gmail.com>
2022-11-22 11:09:33 -0600

    Animations and mobile styling fixes

--- src/lib/Welcome.svelte
+++ src/lib/Welcome.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import { fade, fly } from "svelte/transition";
   import { sineInOut } from "svelte/easing";
   import { spring } from "svelte/motion";
@@ -30,11 +30,16 @@
       }
     }
   }
+
+  let init = false;
+  onMount(() => {
+    init = true;
+  });
 </script>
 
 {#if open && $mouseX !== -142}
   <div
-    class="fixed z-40 inset-6 md:inset-8 rounded-2xl overflow-hidden"
+    class="fixed z-40 inset-3 sm:inset-6 md:inset-8 rounded-2xl overflow-hidden"
     in:fade={{ duration: 200, delay: 200 }}
     out:fade={{ duration: 150 }}
   >
@@ -49,9 +54,9 @@
 
 {#if open}
   <div
-    class="fixed z-40 inset-6 md:inset-8 rounded-2xl
+    class="fixed z-40 inset-3 sm:inset-6 md:inset-8 rounded-2xl
     backdrop-blur-lg border border-gray-300 bg-gray-50/75
-    flex flex-col justify-between items-center px-4 sm:px-6"
+    flex flex-col justify-between items-center px-4 sm:px-6 overflow-y-auto"
     on:mousemove={handleMouse}
     on:wheel={handleWheel}
     in:fade={{ duration: 200 }}
@@ -59,64 +64,73 @@
     bind:clientHeight={height}
     bind:this={welcomeEl}
   >
-    <div class="py-8">
-      <h1 class="text-center text-4xl sm:text-5xl fontvar-heading mb-2 sm:mb-4">
-        dispict
-      </h1>
-      <p class="sm:text-xl">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/ekzhang/dispict#readme">details + code</a
+    {#if init}
+      <div class="py-8">
+        <h2
+          class="text-center text-4xl sm:text-5xl fontvar-heading mb-2 sm:mb-4"
+          in:fade={{ delay: 300 }}
         >
-        <span class="mx-0.5 sm:mx-1.5">|</span>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://harvardartmuseums.org">museum</a
-        >
-        <span class="mx-0.5 sm:mx-1.5">|</span>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/ekzhang1">author</a
+          dispict
+        </h2>
+        <p class="sm:text-xl" in:fade={{ delay: 600 }}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/ekzhang/dispict#readme">details + code</a
+          >
+          <span class="mx-1.5">|</span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://harvardartmuseums.org">museum</a
+          >
+          <span class="mx-1.5">|</span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/ekzhang1">author</a
+          >
+        </p>
+      </div>
+
+      <p
+        class="tagline text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center"
+      >
+        <span in:fade={{ delay: 1200 }}
+          >A growing <span class="text-[105%] font-serif italic"
+            >artistic exhibit</span
+          ></span
+        ><br />
+        <span in:fade={{ delay: 1800 }}
+          >of your <span class="text-[105%] font-serif">own making</span></span
         >
       </p>
-    </div>
 
-    <p
-      class="tagline text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center"
-    >
-      A growing <span class="text-[105%] font-serif italic"
-        >artistic exhibit</span
-      ><br />
-      of your <span class="text-[105%] font-serif">own making</span>
-    </p>
-
-    <div class="py-10">
-      <button
-        class="rounded-full px-5 py-2 bg-black text-white text-lg
+      <div class="py-10" in:fade={{ delay: 2600 }}>
+        <button
+          class="rounded-full px-5 py-2 bg-black text-white text-lg
         hover:bg-white hover:text-black hover:ring-1 hover:ring-black
         active:bg-rose-100 active:text-black active:ring-1 active:ring-black transition-colors"
-        on:click={() => dispatch("close")}
-      >
-        Explore the Gallery
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="inline pb-0.5 ml-1 -mr-1"
-          ><line x1="12" y1="5" x2="12" y2="19" /><polyline
-            points="19 12 12 19 5 12"
-          /></svg
+          on:click={() => dispatch("close")}
         >
-      </button>
-    </div>
+          Explore the Gallery
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="inline pb-0.5 ml-1 -mr-1"
+            ><line x1="12" y1="5" x2="12" y2="19" /><polyline
+              points="19 12 12 19 5 12"
+            /></svg
+          >
+        </button>
+      </div>
+    {/if}
   </div>
 {/if}
 
